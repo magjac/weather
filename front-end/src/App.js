3341d78e6ec9c6244cdb59a883995235e33e8679
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2023-05-21 19:48:13 +0200

    render chart by fetching data file from server

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,17 +1,45 @@
+import * as d3 from "d3";
 import * as React from 'react';
 import Chart from "./Chart.js"
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 
+async function fetchData() {
+  const data = await d3.dsv(",", "http://localhost:8000/Utomhus_2023-05-15_min_max3.csv", (d) => {
+    return {
+      date: new Date(d.date),
+      variant: d.variant,
+      temperature: d.temperature,
+    };
+  });
+  return data;
+}
+
+let initialized = false;
+
 export default function App() {
+  let [temp, setTemp] = React.useState([]);
+  let [dummy, setDummy] = React.useState(1);
+
+  if (!initialized) {
+    fetchData().then((data) => {
+      setTemp(data);
+      setDummy(dummy + 1);
+    });
+    initialized = true;
+  }
+
   return (
     <Container maxWidth="sm">
       <Box sx={{ my: 4 }}>
         <Typography variant="h4" component="h1" gutterBottom>
           Emil´s Weather Station @ bergetvidhandfatet
         </Typography>
-        <Chart />
+        <Chart
+          data={temp}
+          dummy={dummy}
+        />
       </Box>
     </Container>
   );
--- front-end/src/Chart.js
+++ front-end/src/Chart.js
@@ -2,41 +2,16 @@ import * as d3 from "d3";
 import LineChart from "./linechart.js"
 import * as React from 'react';
 
-const data = [
-  {
-    variant: "min",
-    date: new Date("2023-02-15"),
-    temperature: 2.6,
-  },
-  {
-    variant: "min",
-    date: new Date("2023-02-16"),
-    temperature: 2.9,
-  },
-  {
-    variant: "min",
-    date: new Date("2023-02-17"),
-    temperature: 3.9,
-  },
-  {
-    variant: "max",
-    date: new Date("2023-02-15"),
-    temperature: 6.6,
-  },
-  {
-    variant: "max",
-    date: new Date("2023-02-16"),
-    temperature: 11.7,
-  },
-  {
-    variant: "max",
-    date: new Date("2023-02-17"),
-    temperature: 12.9,
-  },
-];
-
 export default class Chart extends React.Component {
   componentDidMount() {
+    this.drawChart();
+  }
+  componentDidUpdate() {
+    this.drawChart();
+  }
+
+  drawChart() {
+    const {data} = this.props;
     const svg = LineChart(data, {
       x: d => d.date,
       y: d => d.temperature,
@@ -47,7 +22,13 @@ export default class Chart extends React.Component {
       color: "steelblue",
     });
     const div = d3.select("#chart");
-    div.node().appendChild(svg);
+    const existingSvg = div.select("svg");
+    if (existingSvg.size() == 0) {
+      div.node().appendChild(svg);
+    }
+    else {
+      div.node().replaceChild(svg, existingSvg.node());
+    }
   }
 
   render() {
