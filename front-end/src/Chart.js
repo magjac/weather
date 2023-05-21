e11d7a0a8031c2f26d66b588161077a7199f935a
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2023-05-21 17:25:32 +0200

    add rendering of a chart with fixed data

--- front-end/src/Chart.js
+++ front-end/src/Chart.js
@@ -1,10 +1,60 @@
-import Typography from '@mui/material/Typography';
+import * as d3 from "d3";
+import LineChart from "./linechart.js"
+import * as React from 'react';
 
-export default function Chart() {
+const data = [
+  {
+    variant: "min",
+    date: new Date("2023-02-15"),
+    temperature: 2.6,
+  },
+  {
+    variant: "min",
+    date: new Date("2023-02-16"),
+    temperature: 2.9,
+  },
+  {
+    variant: "min",
+    date: new Date("2023-02-17"),
+    temperature: 3.9,
+  },
+  {
+    variant: "max",
+    date: new Date("2023-02-15"),
+    temperature: 6.6,
+  },
+  {
+    variant: "max",
+    date: new Date("2023-02-16"),
+    temperature: 11.7,
+  },
+  {
+    variant: "max",
+    date: new Date("2023-02-17"),
+    temperature: 12.9,
+  },
+];
+
+export default class Chart extends React.Component {
+  componentDidMount() {
+    const svg = LineChart(data, {
+      x: d => d.date,
+      y: d => d.temperature,
+      z: d => d.variant,
+      yLabel: "Temperature",
+      width: 800,
+      height: 500,
+      color: "steelblue",
+    });
+    const div = d3.select("#chart");
+    div.node().appendChild(svg);
+  }
+
+  render() {
     return (
-      <Typography variant="body2" color="text.secondary" align="center">
-        <div>
-        </div>
-      </Typography>
-    );
+      <React.Fragment>
+        <div id="chart"></div>
+      </React.Fragment>
+      );
+    }
   }
